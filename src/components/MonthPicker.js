263c38f5530d1a335b0e4eaa9ea51619512ead65
dobeco <dobeco@163.com>
2019-08-28 12:12:34 +0800

    jsmu

--- src/components/MonthPicker.js
+++ src/components/MonthPicker.js
@@ -6,90 +6,90 @@ class MonthPicker extends React.Component {
     super(props)
     this.state = {
       isOpen: false,
-      selectedYear: this.props.year,
+      selectedYear: this.props.year
     }
-    this.toggleDropdown = this.toggleDropdown.bind(this);
   }
-
-  toggleDropdown(event) {
-    event.preventDefault();
+  componentDidMount() {
+    document.addEventListener('click', this.handleClick, false)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClick, false)
+  }
+  handleClick = (event) => {
+    if (this.node.contains(event.target)) {
+      return;
+    }
+    this.setState({
+      isOpen: false,
+    })
+  }
+  toggleDropdown = (event) => {
+    event.preventDefault()
     this.setState({
       isOpen: !this.state.isOpen
     })
-
-  };
+  }
   selectYear = (event, yearNumber) => {
-    event.preventDefault();
+    event.preventDefault()
     this.setState({
       selectedYear: yearNumber
     })
-
   }
   selectMonth = (event, monthNumber) => {
-    event.preventDefault();
+    event.preventDefault()
     this.setState({
       isOpen: false
-    });
-    this.props.onChange(this.state.selectedYear, monthNumber);
+    })
+    this.props.onChange(this.state.selectedYear, monthNumber)
   }
-
   render() {
-    const { year, month } = this.props;
-    const { isOpen, selectedYear } = this.state;
-
-    const monthRange = range(12, 1);
-    const yearRange = range(9, -4).map((number) => { return number + year });
+    const { year, month } = this.props
+    const { selectedYear, isOpen } = this.state;
+    const monthRange = range(12, 1)
+    const yearRange = range(9, -4).map(number => number + year)
     return (
-      <div className=" dropdown month-picker-component">
-        <h4>选择月份</h4>
-        <button className="btn btn-lg btn-secondary dropdown-toggle"
+      <div className="dropdown month-picker-component" ref={(ref) => { this.node = ref }}>
+        <p>选择月份</p>
+        <button
+          className="btn btn-lg btn-secondary dropdown-toggle"
           onClick={this.toggleDropdown}
         >
-          {`${year}年 ${padLeft(month)} 月`}
-
+          {`${year}年 ${padLeft(month)}月`}
         </button>
-        {
-          isOpen &&
-          <div className='dropdown-menu' style={{ display: 'inline-block' }}>
+        {isOpen &&
+          <div className="dropdown-menu" style={{ display: 'block' }}>
             <div className="row">
-              <div className="col border-right">
-                {
-                  yearRange.map((yearNumber, index) =>
-                    <a href="#"
-                      key={index}
-                      onClick={(e) => { this.selectedYear = this.selectYear(e, yearNumber) }}
-                      className={(yearNumber === selectedYear) ? 'dropdown-item active' : 'dropdown-item'}>{yearNumber} 年</a>
-                  )
-                }
+              <div className="col border-right years-range">
+                {yearRange.map((yearNumber, index) =>
+                  <a key={index}
+                    role="button"
+                    onClick={(event) => { this.selectYear(event, yearNumber) }}
+                    className={(yearNumber === selectedYear) ? "dropdown-item active text-white" : "dropdown-item"}>
+                    {yearNumber} 年
+                  </a>
+                )}
               </div>
-              <div className="col">
-                {
-                  monthRange.map((monthNumber, index) =>
-                    <a href='#'
-                      key={index}
-                      onClick={(e) => { this.selectMonth(e, monthNumber) }}
-                      className={(monthNumber === month) ? 'dropdown-item active' : 'dropdown-item'}>
-                      {padLeft(monthNumber)}月
-                    </a>
-                  )
-                }
+              <div className="col months-range">
+                {monthRange.map((monthNumber, index) =>
+                  <a key={index}
+                    role="button"
+                    onClick={(event) => { this.selectMonth(event, monthNumber) }}
+                    className={(monthNumber === month) ? "dropdown-item active text-white" : "dropdown-item"}>
+                    {padLeft(monthNumber)} 月
+                  </a>
+                )}
               </div>
             </div>
           </div>
         }
-
-
       </div>
-
-
     )
   }
-
 }
+
 MonthPicker.propTypes = {
   year: PropTypes.number.isRequired,
   month: PropTypes.number.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
 }
-
 export default MonthPicker
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,39 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Ionicon from 'react-ionicons'
-import logo from '../../logo.svg'
-import { LIST_VIEW, CHART_VIEW, TYPE_INCOME, TYPE_OUTCOME, parseToYearAndMonth } from '../../utility'
-import CreateBtn from '../../components/CreateBtn'
-import ViewTab from '../../components/ViewTab'
-import PriceList from '../../components/PriceList'
-import MonthPicker from '../../components/MonthPicker'
-import TotalPrice from '../../components/TotalPrice'
+import React from "react";
+import PropTypes from "prop-types";
+import Ionicon from "react-ionicons";
+import logo from "../../logo.svg";
+import {
+  LIST_VIEW,
+  CHART_VIEW,
+  TYPE_INCOME,
+  TYPE_OUTCOME,
+  parseToYearAndMonth,
+  padLeft
+} from "../../utility";
+import CreateBtn from "../../components/CreateBtn";
+import ViewTab from "../../components/ViewTab";
+import PriceList from "../../components/PriceList";
+import MonthPicker from "../../components/MonthPicker";
+import TotalPrice from "../../components/TotalPrice";
 
 const items = [
   {
-    "id": 1,
-    "title": "去云南旅游",
-    "price": 200,
-    "date": "2018-09-10",
-    "cid": 1
-
+    id: 1,
+    title: "去云南旅游",
+    price: 200,
+    date: "2019-08-20",
+    cid: 1
   },
   {
-    "id": 2,
-    "title": "去云南旅游",
-    "price": 400,
-    "date": "2018-09-10",
-    "cid": 1
+    id: 2,
+    title: "去云南旅游",
+    price: 400,
+    date: "2018-09-10",
+    cid: 1
   },
   {
-    "id": 3,
-    "title": "理财收入",
-    "price": 500,
-    "date": "2018-09-10",
-    "cid": 2
+    id: 3,
+    title: "理财收入",
+    price: 500,
+    date: "2019-08-10",
+    cid: 2
   }
-
-
 ];
 
 const categories = {
@@ -42,51 +46,99 @@ const categories = {
     name: "旅行",
     type: "outcome",
     iconName: "ios-plane"
-
   },
   "2": {
     id: "2",
     name: "理财",
     type: "income",
     iconName: "logo-yen"
-
   }
+};
 
-}
-
+const newItem = {
+  id: 4,
+  title: "新添加的项目",
+  price: 300,
+  date: "2018-10-10",
+  cid: 1
+};
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items,
-      currentDate: parseToYearAndMonth(),
+      currentDate: parseToYearAndMonth(), // 当前年和月
       tabView: LIST_VIEW
-
-    }
+    };
   }
 
   componentDidMount() {
-    console.log(this.state.currentDate)
+    console.log(this.state.currentDate);
   }
+
+  // 模式切换
+  changeView = view => {
+    this.setState({ tabView: view });
+  };
+  changeDate = (year, month) => {
+    this.setState({
+      currentData: { year, month }
+    });
+  };
+  // 修改item
+  modifyItem = modifiedItem => {
+    const modifiedItems = this.state.items.map(item => {
+      if (item.id === modifiedItem.id) {
+        return { ...item, title: "更新后的标题" };
+      } else {
+        return item;
+      }
+    });
+    this.setState({
+      items: modifiedItems
+    });
+  };
+
+  createItem = () => {
+    this.setState({
+      items: [newItem, ...this.state.items]
+    });
+  };
+
+  deleteItem = deleteItem => {
+    const filteredItems = this.state.items.filter(
+      item => item.id !== deleteItem.id
+    );
+    this.setState({
+      items: filteredItems
+    });
+  };
 
   render() {
     const { items, currentDate, tabView } = this.state;
-    const itemsWithCategory = items.map(item => {
-      item.category = categories[item.cid]
-      return item
-    })
-    let totalIncome = 0, totalOutcome = 0;
+    const itemsWithCategory = items
+      .map(item => {
+        item.category = categories[item.cid];
+        return item;
+      })
+      .filter(item => {
+        return item.date.includes(
+          currentDate.year + '-' + padLeft(currentDate.month)
+        );
+      });
+    let totalIncome = 0,
+      totalOutcome = 0;
     itemsWithCategory.forEach(item => {
       if (item.category.type === TYPE_OUTCOME) {
-        totalOutcome += item.price
+        totalOutcome += item.price;
       } else {
-        totalIncome += item.price
+        totalIncome += item.price;
       }
-    })
+    });
 
     return (
-      < React.Fragment >
+      <React.Fragment>
         <div className="App-header">
           <div className="row mb-5 justify-content-center">
             <img src={logo} className="App-logo" alt="logo"></img>
@@ -97,35 +149,30 @@ class Home extends React.Component {
               <MonthPicker
                 year={currentDate.year}
                 month={currentDate.month}
-                onchange={(e) => { console.log(e) }}
+                onChange={this.changeDate}
               />
             </div>
           </div>
 
           <div className="col">
             <TotalPrice income={totalIncome} outcome={totalOutcome} />
-
           </div>
         </div>
 
         <div className="content-area py-3 px-3">
-          <ViewTab activeTab={tabView} onTabChange={() => { }} />
-          <CreateBtn onClick={() => { }} />
-          <PriceList
-            items={itemsWithCategory}
-            onModifyItem={item => {
-              console.log(item.id);
-            }}
-            onDeleteItem={item => {
-              console.log("delete:" + item.id);
-            }}
-          />
-
+          <ViewTab activeTab={tabView} onTabChange={this.changeView} />
+          <CreateBtn onClick={this.createItem} />
+          {tabView === LIST_VIEW && (
+            <PriceList
+              items={itemsWithCategory}
+              onModifyItem={this.modifyItem}
+              onDeleteItem={this.deleteItem}
+            />
+          )}
+          {tabView === CHART_VIEW && <h1>图标</h1>}
         </div>
-
-
-      </React.Fragment >
-    )
+      </React.Fragment>
+    );
   }
 }
 
